774b8afef5bd8ea963553c346764d4c0045e010a
John Reiter <reiterj@upmc.edu>
2017-06-26 16:38:54 -0400

    Use more default options from prettier

--- index.js
+++ index.js
@@ -21,18 +21,8 @@ module.exports = function(opt) {
 
     options = merge(
       {
-        // Fit code within this line limit
-        printWidth: 80,
-        // Number of spaces it should use per tab
-        tabWidth: 2,
-        // Use the flow parser instead of babylon
-        useFlowParser: true,
         // If true, will use single instead of double quotes
-        singleQuote: false,
-        // Controls the printing of trailing commas wherever possible
-        trailingComma: false,
-        // Controls the printing of spaces inside array and objects
-        bracketSpacing: true
+        singleQuote: false
       },
       opt
     );
